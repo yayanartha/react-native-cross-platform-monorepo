4a09e727724e5711157ad15f528935440aae1d37
Yayan Artha <>
2023-01-15 22:21:15 +0800

    add apps/mobile

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,21 @@
+import 'expo-dev-client'
+import { NativeNavigation } from 'app/navigation/mobile'
+import { Provider } from 'app/provider'
+import { useFonts } from 'expo-font'
+
+export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <Provider>
+      <NativeNavigation />
+    </Provider>
+  )
+}
